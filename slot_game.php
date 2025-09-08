eb407cdacd5128c5b326c809538f03737ab3f6f1
Joshua <jl0977287@otc.edu>
2025-09-08 00:34:10 -0500

    Better spot

--- slot_game.php
+++ slot_game.php
@@ -3,30 +3,19 @@ $letters = ['A', 'B', 'C'];
 $spin = 0;
 $money = 0;
 
-echo "Win 100 credits by spinning three identical symbols!\nWin 50 credits by spinning two identical symbols and one different!";
 while ($spin < 20 && $money <= 500) {
+    $spinResult = '';
     for ($x = 0; $x < 3; $x++) {
-        $slotOne = $letters[array_rand($letters)];
-        $slotTwo = $letters[array_rand($letters)];
-        $slotThree = $letters[array_rand($letters)];
-        $slotTotal = $slotOne . $slotTwo . $slotThree;
-        echo $slotTotal;
-        $result = 0;
-        $result = match ($slotTotal) {
-            'AAA', 'BBB', 'CCC' => $result + 100,
-            'AAB', 'ABA', 'BAA', 'ABB', 'BBA', 'BAB', 'BCC', 'CBC', 'CCB', 'ACC', 'CAC', 'CCA' => $result + 50,
-            default => $result,
-        };
-        $money += $result;
-         if ($result == 100) {
-            echo " You won 100 credits!\n";
-        } else if ($result == 50) {
-            echo " You won 50 credits!\n";
-        } else{
-            echo " You won 0 credits.\n";
-        }
+        $randomLetter = array_rand($letters);
+        $spinResult .= $letters[$randomLetter];
     }
+    echo $spinResult . "\n";
+    $result = 0;
+    $money = match ($spinResult) {
+        'AAA', 'BBB', 'CCC' => $money + 100,
+        'AAB', 'ABA', 'BAA', 'ABB', 'BBA', 'BAB', 'BCC', 'CBC', 'CCB', 'ACC', 'CAC', 'CCA' => $money + 50,
+        default => $money,
+    };
+    echo $money;
     $spin++;
 }
-
-echo $money;
